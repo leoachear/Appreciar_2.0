274c4cf6624226ad7602989c853d0741157bf050
@LeoAchear <leoachear@gmail.com>
2016-04-16 14:07:55 -0300

    Service, Ng-repeat, Firebase y Cards funcionando
    Corregido el error que no cargaba los datos.  Se agrega un service y se
    cargan los datos con el controlador en el $rootScope.
    Se agregan los cards y se le empieza a dar forma.

--- app/src/users/UserService.js
+++ app/src/users/UserService.js
@@ -17,16 +17,19 @@
       {
         name: 'Ultimas Actualizaciones',
         avatar: 'svg-1',
+        dato: 'Posts',
         //content: 'I love cheese, especially airedale queso. Cheese and biscuits halloumi cauliflower cheese cottage cheese swiss boursin fondue caerphilly. Cow port-salut camembert de normandie macaroni cheese feta who moved my cheese babybel boursin. Red leicester roquefort boursin squirty cheese jarlsberg blue castello caerphilly chalk and cheese. Lancashire.'
       },
       {
         name: 'Por Zona',
         avatar: 'svg-2',
+        dato: 'Zonas',
         //content: 'Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis. Summus brains sit​​, morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum mauris.'
       },
       {
         name: 'Por Productos',
         avatar: 'svg-3',
+        dato: 'Productos',
         //content: "Raw denim pour-over readymade Etsy Pitchfork. Four dollar toast pickled locavore bitters McSweeney's blog. Try-hard art party Shoreditch selfies. Odd Future butcher VHS, disrupt pop-up Thundercats chillwave vinyl jean shorts taxidermy master cleanse letterpress Wes Anderson mustache Helvetica. Schlitz bicycle rights chillwave irony lumberhungry Kickstarter next level sriracha typewriter Intelligentsia, migas kogi heirloom tousled. Disrupt 3 wolf moon lomo four loko. Pug mlkshk fanny pack literally hoodie bespoke, put a bird on it Marfa messenger bag kogi VHS."
       }/*,
       {
