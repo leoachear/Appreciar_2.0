244aab643ea7fbe7a08c5ef15cb8c23ded731608
@LeoAchear <leoachear@gmail.com>
2016-04-21 04:13:36 -0300

    Posteo por Zonas
    Se agrega en campo Zonas para un posteo.
    Cuando se completa el campo zonas, aparte de agregar el Post en el padre
    Posts, también se agrega en PostsZonas.

--- app/src/users/UserService.js
+++ app/src/users/UserService.js
@@ -17,25 +17,26 @@
       {
         name: 'Ultimas Actualizaciones',
         avatar: 'svg-1',
-        dato: 'Posts',
+        dato: 'Posts'
         //content: 'I love cheese, especially airedale queso. Cheese and biscuits halloumi cauliflower cheese cottage cheese swiss boursin fondue caerphilly. Cow port-salut camembert de normandie macaroni cheese feta who moved my cheese babybel boursin. Red leicester roquefort boursin squirty cheese jarlsberg blue castello caerphilly chalk and cheese. Lancashire.'
       },
       {
         name: 'Por Zona',
         avatar: 'svg-2',
-        dato: 'Zonas',
+        dato: 'Zonas'
         //content: 'Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis. Summus brains sit​​, morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum mauris.'
       },
       {
         name: 'Por Productos',
         avatar: 'svg-3',
-        dato: 'Productos',
+        dato: 'Productos'
         //content: "Raw denim pour-over readymade Etsy Pitchfork. Four dollar toast pickled locavore bitters McSweeney's blog. Try-hard art party Shoreditch selfies. Odd Future butcher VHS, disrupt pop-up Thundercats chillwave vinyl jean shorts taxidermy master cleanse letterpress Wes Anderson mustache Helvetica. Schlitz bicycle rights chillwave irony lumberhungry Kickstarter next level sriracha typewriter Intelligentsia, migas kogi heirloom tousled. Disrupt 3 wolf moon lomo four loko. Pug mlkshk fanny pack literally hoodie bespoke, put a bird on it Marfa messenger bag kogi VHS."
       }/*,
       {
-        name: 'Lawrence Ray',
+        name: 'Postear',
         avatar: 'svg-4',
-        content: 'Scratch the furniture spit up on light gray carpet instead of adjacent linoleum so eat a plant, kill a hand pelt around the house and up and down stairs chasing phantoms run in circles, or claw drapes. Always hungry pelt around the house and up and down stairs chasing phantoms.'
+        dato: 'Postear'
+        //content: 'Scratch the furniture spit up on light gray carpet instead of adjacent linoleum so eat a plant, kill a hand pelt around the house and up and down stairs chasing phantoms run in circles, or claw drapes. Always hungry pelt around the house and up and down stairs chasing phantoms.'
       },
       {
         name: 'Ernesto Urbina',
